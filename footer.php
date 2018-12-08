ec1369c2fa67ac45994b6691d37b188a22e96152
Andrey <soul.evans.15@gmail.com>
2018-12-08 19:01:29 +0300

    deleted ezPieChart from footer

--- footer.php
+++ footer.php
@@ -10,7 +10,6 @@
 <link rel="stylesheet" href="<?=DS_ROOT . '/font-awesome/css/font-awesome.min.css'?>">
 <link rel="stylesheet" href="<?=DS_ROOT . '/style.css'?>">
 <script src="<?=DS_ROOT . '/js/jquery-3.3.1.min.js'?>"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.6/jquery.easypiechart.min.js"></script>
 <script src="<?=DS_ROOT . '/js/script.js'?>"></script>
 <!--    MINIFIED     -->
 <!--<link rel="stylesheet" href="--><?//=DS_ROOT . '/minified/index.css'?><!--">-->
