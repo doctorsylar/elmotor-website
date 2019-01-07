4fa7b29a830b40c131c37528b3b8a19921a97064
Andrey <33748355+doctorsylar@users.noreply.github.com>
2019-01-07 14:40:03 +0300

    last footer changes

--- footer.php
+++ footer.php
@@ -105,3 +105,5 @@ $pageName =  $addressArray[count($addressArray) - 1];
 <script src="<?=DS_ROOT . '/js/jquery-3.3.1.min.js'?>"></script>
 <script src="<?=DS_ROOT . '/js/script.js'?>"></script>
 <script src="<?=DS_ROOT . "/js/$pageName-script.js"?>"></script>
+<!-- Yandex.Metrika counter --> 
+<script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(51683804, "init", { id:51683804, clickmap:true, trackLinks:true, accurateTrackBounce:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/51683804" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
