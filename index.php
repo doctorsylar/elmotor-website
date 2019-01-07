652b83b6eb60edd1506ac36a96e38d5b5a4082fc
Andrey <33748355+doctorsylar@users.noreply.github.com>
2019-01-07 16:11:01 +0300

    index updated

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 
     <main>
         <div class="container">
-            Такой страницы не существует
+            <p>К сожалению, такой страницы не существует. Она была удалена или перемещена.</p>
+            <p>Вы будете перенаправлены на главную страницу через несколько секунд.</p>
         </div>
     </main>
 
 <?php get_footer(); ?>
+<script>
+    setTimeout(function () {
+        window.location.href = "http://elmtr.ru";
+    }, 5000);
+</script>
